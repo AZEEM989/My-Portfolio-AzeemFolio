e6518bbc26d9c8543a3897dc96a8435cba236f63
AZEEM989 <azeemurrehman68@gmail.com>
2024-09-28 11:53:36 +0530

    add all files

---
+++ port.js
@@ -0,0 +1,8 @@
+let main=document.querySelector(".main");
+let contactMe=document.querySelector(".conme");
+
+contactMe.addEventListener("click",()=>{
+    document.querySelector(".reach").scrollIntoView({
+    behavior: 'smooth'
+})
+})
